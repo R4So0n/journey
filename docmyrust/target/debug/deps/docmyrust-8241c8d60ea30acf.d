2070c2a4219972839d5fbe034b29bb896d72643b
λ l e x a n d e r <hsa.lapaz@gmail.com>
2024-10-17 23:24:59 -0700

    Delete docmyrust/target directory

--- docmyrust/target/debug/deps/docmyrust-8241c8d60ea30acf.d
+++
@@ -1,5 +0,0 @@
-/Users/retbleed/Projects/rasoon/journey/docmyrust/target/debug/deps/libdocmyrust-8241c8d60ea30acf.rmeta: src/lib.rs
-
-/Users/retbleed/Projects/rasoon/journey/docmyrust/target/debug/deps/docmyrust-8241c8d60ea30acf.d: src/lib.rs
-
-src/lib.rs:
--- docmyrust/target/debug/deps/docmyrust-f1696dd059db1d25.d
+++
@@ -1,5 +0,0 @@
-/Users/retbleed/Projects/rasoon/journey/docmyrust/target/debug/deps/libdocmyrust-f1696dd059db1d25.rmeta: src/lib.rs
-
-/Users/retbleed/Projects/rasoon/journey/docmyrust/target/debug/deps/docmyrust-f1696dd059db1d25.d: src/lib.rs
-
-src/lib.rs:
